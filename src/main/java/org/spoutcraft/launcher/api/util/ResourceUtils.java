92e58b2b4a37378c4d9221965c3131e07bd5e93a
Milan <Milan@Srstka>
2012-09-21 18:40:23 +0200

    Merge from https://github.com/SpoutDev/SpoutcraftLauncher/ Moved code around, killed dead classes, and broke the launcher. commit 0f13def9771bfa1314052f1d73a63a70cc8287c8 1 parent 23b5ce5e4d Afforess authored 2 days ago

--- src/main/java/org/spoutcraft/launcher/api/util/ResourceUtils.java
+++ src/main/java/org/spoutcraft/launcher/api/util/ResourceUtils.java
@@ -24,29 +24,3 @@
  * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
  * including the MIT license.
  */
-package org.spoutcraft.launcher.api.util;
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-public class ResourceUtils {
-
-	public static InputStream getResourceAsStream(String path) {
-		InputStream stream = ImageUtils.class.getResourceAsStream(path);
-		String[] split = path.split("/");
-		path = split[split.length - 1];
-		if (stream == null) {
-			File resource = new File(".\\src\\main\\resources\\" + path);
-			if (resource.exists()) {
-				try {
-					stream = new BufferedInputStream(new FileInputStream(resource));
-				} catch (IOException ignore) { }
-			}
-		}
-		return stream;
-	}
-	
-}
